bc3a225670f1b6bdd073f1098b4294db7325b6c2
amclai10 <amclai10@charlotte.edu>
2025-11-10 18:23:53 -0500

    title fix

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -17,9 +17,9 @@ export default function Footer() {
             <ul className="footer-links">
                 <li><a href="https://github.com/amclai10" target="_blank">GitHub</a></li>
                 <li><a href="https://www.linkedin.com/in/aidan-mclain/" target="_blank">LinkedIn</a></li>
-                <li><a href="https://webpages.charlotte.edu/amclai10/" target="_blank">Charlotte Webpage Home</a></li>
-                <li><a href="https://webpages.charlotte.edu/amclai10/itis3135" target="_blank">Charlotte ITIS3135 Page</a></li>
-                <li><a href="https://amclai10.github.io/amclai10/" target="_blank">GitHub page</a></li>
+                <li><a href="https://webpages.charlotte.edu/amclai10/" target="_blank">CLT Webpage Home</a></li>
+                <li><a href="https://webpages.charlotte.edu/amclai10/itis3135" target="_blank">CLT ITIS3135 Page</a></li>
+                <li><a href="https://amclai10.github.io/amclai10/" target="_blank">GitHub.io Page</a></li>
                 <li><a href="aidanmc.com/">Designed by Aidan McLain LLC</a></li>
                 <li><a href="ambitiousMonkey/">Ambitious Monkey</a></li>
                 <li><a ref={htmlLinkRef} target="_blank" rel="noopener noreferrer">Validate HTML</a></li>
