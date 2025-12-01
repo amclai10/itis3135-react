0ecd568ee3e0e80c9317cc260a004b20df61fc49
amclai10 <amclai10@charlotte.edu>
2025-11-30 22:12:43 -0500

    studentintrosinitial

--- src/StudentIntroductions.jsx
+++ src/StudentIntroductions.jsx
@@ -15,12 +15,12 @@ export default function ClassIntros() {
             .then((data) => setIntroductionData(data))
             .catch(error => setError(error.message));
     }, [])
-    return <>
+    return <main>
         <h2>Everybody's Introductions</h2>
         {error == null ? "" : <p>Error Code {error}</p>}
         {
             introductionData.map((student, index) =>
-                <article key={index}>
+                <article key={index} className ="studentIntros">
                     <h3>
                         {student.name.first} {" "}
                         {student.name.middleInitial ? student.name.middleInitial + ". " : ""}  {" "}
@@ -28,36 +28,48 @@ export default function ClassIntros() {
                         {student.name.last} {" "}
                         {student.divider} {" "}
                     </h3>
-                    <figure>
-                        <img src={"https://dvonb.xyz" + student.media.srd} />
-                        <figcaption>{student.media.caption}</figcaption>
-                    </figure>
-                    <p>{student.personalStatement}</p>
-                    <ul>
-                        <li><strong>Personal Background: </strong> {student.backgrounds.personal}</li>
-                        <li><strong>Professional Background: </strong> {student.backgrounds.professional}</li>
-                        <li><strong>Academic Background: </strong> {student.backgrounds.academic}</li>
-                        <li><strong>Subject Background: </strong> {student.backgrounds.subject}</li>
-                        <li>
-                            <strong>Courses:</strong>
-                            <ol>
-                                data.courses.map((course, index) => <li key={index}><strong>{course.dept} {course.num} - {course.name}: {course.reason}</strong></li>)
-                            </ol>
-                        </li>
-                        <li>
-                            <strong>Fun Fact: </strong> {student.funFact}
-                        </li>
-                        <li>
-                            <strong>Additional Information: </strong> {student.additional}
-                        </li>
-                    </ul>
-                    <p><em>{student.quote.text}</em>
-                    <br />- {student.quote.author}</p>
-                    <a href={student.links.charlotte}>CLT Web</a> <a href={student.links.gitHub}>GitHub</a> <a href={student.links.githubio}>Github.io</a> <a href={student.links.itis3135}>ITIS 3135</a> <a href={student.links.freecodecamp}>freeCodeCamp</a><a href={student.links.codecademy}>CodeAcademy</a><a href={student.links.linkedin}>linkedIn</a>
+                    <div className="studentIntros-content">
+                        <figure className="studentIntros-figure">
+                            <img src={"https://dvonb.xyz" + student.media.src} alt={student.media.caption} />
+                            <figcaption>{student.media.caption}</figcaption>
+                        </figure>
+                        <div className="studentIntros-text">
+                            <p>{student.personalStatement}</p>
+                            <ul>
+                                <li><strong>Personal Background: </strong> {student.backgrounds.personal}</li>
+                                <li><strong>Professional Background: </strong> {student.backgrounds.professional}</li>
+                                <li><strong>Academic Background: </strong> {student.backgrounds.academic}</li>
+                                <li><strong>Subject Background: </strong> {student.backgrounds.subject}</li>
+                                <li>
+                                    <strong>Courses:</strong>
+                                    <ol>
+                                        {student.courses.map((course, index) => <li key={index}><strong>{course.dept} {course.num} - {course.name}</strong>: {course.reason}</li>)}
+                                    </ol>
+                                </li>
+                                <li>
+                                    <strong>Fun Fact: </strong> {student.funFact}
+                                </li>
+                                <li>
+                                    <strong>Additional Information: </strong> {student.additional}
+                                </li>
+                            </ul>
+                            <p><em>{student.quote.text}</em>
+                            <br />- {student.quote.author}</p>
+                        </div>
+                    </div>
+                    <div className="studentIntros-links">
+                        <a href={student.links.charlotte}>CLT Web</a> 
+                        <a href={student.links.gitHub}>GitHub</a> 
+                        <a href={student.links.githubio}>Github.io</a> 
+                        <a href={student.links.itis3135}>ITIS 3135</a> 
+                        <a href={student.links.freecodecamp}>freeCodeCamp</a>
+                        <a href={student.links.codecademy}>CodeAcademy</a>
+                        <a href={student.links.linkedin}>linkedIn</a>
+                    </div>
                     <hr />
                 </article>
             )
 
         }
-    </>
+    </main>
 }
